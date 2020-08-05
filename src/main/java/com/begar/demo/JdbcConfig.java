ac1d0b24b71d20011dcdf50f180a03c19b2b4813
IvanBegar <59286416+IvanBegar@users.noreply.github.com>
2020-08-05 12:24:23 +0300

    replaced getConnection with DataSource instead of DriverManager

---
+++ src/main/java/com/begar/demo/JdbcConfig.java
@@ -0,0 +1,39 @@
+package com.begar.demo;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class JdbcConfig {
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String user;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+        @Bean
+        public DataSource getDataSource()
+        {
+            DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+            dataSourceBuilder.url(url);
+            dataSourceBuilder.username(user);
+            dataSourceBuilder.password(password);
+            return dataSourceBuilder.build();
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(@Qualifier("getDataSource") DataSource ds) {
+            return new JdbcTemplate(ds);
+    }
+}
+
