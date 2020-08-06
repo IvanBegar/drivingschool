d79879de72042d39f22386344a5772f86f4b7180
IvanBegar <59286416+IvanBegar@users.noreply.github.com>
2020-08-06 22:40:17 +0300

    doing some code refactoring

---
+++ src/main/java/com/begar/demo/dto/GroupDTO.java
@@ -0,0 +1,14 @@
+package com.begar.demo.dto;
+
+public class GroupDTO {
+
+    private String groupName;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+}
---
+++ src/main/java/com/begar/demo/exception/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.begar.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler {
+
+
+
+}
